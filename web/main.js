9209fd5a6a898df50f0033284f191f3072361ff6
unknown <nguyentien.it97@gmail.com>
2022-01-19 15:48:57 +0700

    thay doi giao dien test

---
+++ web/main.js
@@ -0,0 +1,21 @@
+// Khai bao bien
+/*var fullName = 'Nguyen Dinh Tien';
+var age = 26;
+var sex = Male;
+
+
+// Goi ham thong bao
+alert(fullName);
+alert(age);
+alert(sex);
+
+/** 
+ * Một số hàm console
+ * 
+*/
+var a = 1;
+var b = 2;
+
+var c = a + b;
+
+console.log(c);
